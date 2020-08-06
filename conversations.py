b0d245b8f9acf8933ac73d8a6035ccb193a1f64c
Stepanov Dmitriy <58698274+adminush@users.noreply.github.com>
2020-08-06 22:41:43 +0300

    Add files via upload

--- conversations.py
+++ conversations.py
@@ -3,29 +3,17 @@ from vk_api import VkApi, VkUpload
 from datetime import datetime
 from vk_api.longpoll import VkLongPoll
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from toks import mainToken, idGroup
 
-print("Welcome to:        ___                         ___                 ")
-print("      ___         /__/|         _____         /  /\          ___   ")
-print("     /__/\       |  |:|        /  /::\       /  /::\        /  /\  ")
-print("     \  \:\      |  |:|       /  /:/\:\     /  /:/\:\      /  /:/  ")
-print("      \  \:\   __|  |:|      /  /:/~/::\   /  /:/  \:\    /  /:/   ")
-print("  ___  \__\:\ /__/\_|:|____ /__/:/ /:/\:| /__/:/ \__\:\  /  /::\   ")
-print(" /__/\ |  |:| \  \:\/:::::/ \  \:\/:/~/:/ \  \:\ /  /:/ /__/:/\:\  ")
-print(" \  \:\|  |:|  \  \::/~~~~   \  \::/ /:/   \  \:\  /:/  \__\/  \:\ ")
-print("  \  \:\__|:|   \  \:\        \  \:\/:/     \  \:\/:/        \  \:\ ")
-print("   \  \::::/     \  \:\        \  \::/       \  \::/          \__\/")
-print("    \__\__/       \__\/         \__\/         \__\/              \n")
-print("Fuc$*ng bot for conversations! \n     1. Comands: 'Старт', 'Бот инфо'   2. With users: none")
+print("\n\nWelcome to:        ___                            ___                 \n      ___         /__/|            _____         /  /\          ___   \n     /__/\       |  |:|           /  /::\       /  /::\        /  /\  \n     \  \:\      |  |:|          /  /:/\:\     /  /:/\:\      /  /:/  \n      \  \:\   __|  |:|         /  /:/~/::\   /  /:/  \:\    /  /:/   \n  ___  \__\:\ /__/\_|:|____    /__/:/ /:/\:| /__/:/ \__\:\  /  /::\   \n /__/\ |  |:| \  \:\/:::::/    \  \:\/:/~/:/ \  \:\ /  /:/ /__/:/\:\  \n \  \:\|  |:|  \  \::/___/      \  \::/ /:/   \  \:\  /:/  \__\/  \:\ \n  \  \:\__|:|   \  \:\           \  \:\/:/     \  \:\/:/        \  \:\ \n   \  \::::/     \  \:\           \  \::/       \  \::/          \__\/\n    \__\__/       \__\/            \__\/         \__\/              \nFuc$*ng bot for conversations! \n1. Comands: '/Старт', 'Бот инфо'   2. With users: none\n\n")
 
-mainToken = "ups"
-idGroup = "ups"
-
-vkSession = VkApi(token=mainToken)
+vkSession = VkApi(token = mainToken)
 vk = vkSession.get_api()
 longpoll = VkBotLongPoll(vkSession, idGroup)
 
 upload = VkUpload(vkSession)
 
+start_day = int(1)
 minutka = 0
 
 for event in longpoll.listen():
@@ -39,9 +27,11 @@ for event in longpoll.listen():
 			chat_id = int(event.chat_id)
 
 			vkMessage = "Всем привет, я бот ! Developer, но меня можно звать просто, бот."
-			vk.messages.send ( random_id=random_id, chat_id=chat_id, message=vkMessage, )
+			print("Comand -> 'Бот инфо'")
+			vk.messages.send ( random_id = random_id, chat_id = chat_id, message = vkMessage, )
 
-		if text.lower() == 'старт':
+		if text.lower() == '/старт':
+			print("Comand -> '/Старт'")
 			random_id = round(random() * 10 ** 9)
 			chat_id = int(event.chat_id)
 
@@ -51,19 +41,25 @@ for event in longpoll.listen():
 					current_datetime = datetime.now()
 
 					if minutka != current_datetime.minute:
-						print("Real time is: ", current_datetime.hour, ":" ,current_datetime.minute)
+						print("Real time is: ", current_datetime.hour, ":" ,current_datetime.minute, " ", start_day)
+						set_day = 1
 
 					minutka = current_datetime.minute
 
+					if current_datetime.hour == 16 and current_datetime.minute == 26 and current_datetime.second == 0 and set_day == 1:
+						start_day += 1
+						print("Number day is: ", start_day)
+
 					# Отправка сообщения утром
-					if current_datetime.hour == 0 and current_datetime.minute == 5 and current_datetime.second < 2:
+					if current_datetime.hour == 18 and current_datetime.minute == 00 and current_datetime.second < 2:
 						vkMessage = "Доброе утро! Расписание на сегодня:\n\n 9:00 - 10:00 Сасите\n 11:00 - 12:00 Идите нахуй"
-						vk.messages.send ( random_id=random_id, chat_id=chat_id, message=vkMessage, )
+						vk.messages.send ( random_id = random_id, chat_id = chat_id, message = vkMessage, )
 
 					# Отправка сообщения вечером
-					if current_datetime.hour == 20 and current_datetime.minute == 00 and current_datetime.second < 2:
-						vkMessage = "Доброе утро!"
-						vk.messages.send ( random_id=random_id, chat_id=chat_id, message=vkMessage, )
+					if current_datetime.hour == 18 and current_datetime.minute == 30 and current_datetime.second < 2:
+						vkMessage = "Доброе вечер! Расписание на завтра:\n\n 9:00 - 10:00 Умри от спид\n 11:00 - 12:00 Будь куколдом!"
+						vk.messages.send ( random_id = random_id, chat_id = chat_id, message = vkMessage, )
 
+					set_day = 0
 				except Exception as e:
 					print("stop")
